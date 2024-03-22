f6321fc8d61afc9937bb07dbc85048a8db096b2b
dolong2 <k01066624566@gmail.com>
2024-03-22 21:26:23 +0900

    [add] Music 엔티티 추가

---
+++ src/domain/music/entity/music.entity.ts
@@ -0,0 +1,35 @@
+import { Artist } from "src/domain/artist/entity/artist.entity";
+import { Column, Entity, ManyToOne, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class Music {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  youtubeUrl: string;
+
+  @Column()
+  views: number;
+
+  @Column()
+  viewsOfWeek: number;
+
+  @Column()
+  viewsOfDay: number;
+
+  @Column()
+  viewsOfTime: number;
+
+  @Column()
+  uploadedDate: Date;
+
+  @Column()
+  karaokeCode: number;
+
+  @ManyToOne(() => Artist)
+  artist: Artist;
+}
