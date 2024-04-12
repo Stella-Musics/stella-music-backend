36ba213e2579d55a879e64d9129c2ecb4daf9135
dolong2 <k01066624566@gmail.com>
2024-04-12 23:57:18 +0900

    [add] 주간 차트 엔티티 추가

---
+++ src/domain/chart/entity/chart-of-week.entity.ts
@@ -0,0 +1,13 @@
+import { Music } from "src/domain/music/entity/music.entity";
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class ChartOfWeek {
+  @PrimaryGeneratedColumn()
+  readonly id: number;
+  @ManyToOne(() => Music)
+  readonly music: Music;
+  readonly views: number;
+  readonly ranking: number;
+  readonly createdAt: Date;
+}
