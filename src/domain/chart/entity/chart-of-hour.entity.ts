eb012d650a4a53da2f370eb6334b0071672033a7
dolong2 <k01066624566@gmail.com>
2024-04-13 01:01:55 +0900

    [chore] chart 엔티티와 music 엔티티의 연관관계를 1:1로 수정

--- src/domain/chart/entity/chart-of-hour.entity.ts
+++ src/domain/chart/entity/chart-of-hour.entity.ts
@@ -1,11 +1,12 @@
 import { Music } from "src/domain/music/entity/music.entity";
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class ChartOfHour {
   @PrimaryGeneratedColumn()
   readonly id: number;
-  @ManyToOne(() => Music)
+  @OneToOne(() => Music)
+  @JoinColumn()
   readonly music: Music;
   readonly views: number;
   readonly ranking: number;
