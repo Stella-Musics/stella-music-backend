0f41c093c8a93e406aaa286cc07785e6fba2f45e
dolong2 <k01066624566@gmail.com>
2024-04-14 21:52:03 +0900

    [add] 연간 차트 엔티티 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ChartOfWeek } from "./domain/chart/entity/chart-of-week.entity";
 import { ChartOfDay } from "./domain/chart/entity/chart-of-day.entity";
 import { ChartOfHour } from "./domain/chart/entity/chart-of-hour.entity";
 import { ChartOfMonth } from "./domain/chart/entity/chart-of-month.entity";
+import { ChartOfYear } from "./domain/chart/entity/chart-of-year.entity";
 
 @Module({
   imports: [
@@ -23,7 +24,16 @@ import { ChartOfMonth } from "./domain/chart/entity/chart-of-month.entity";
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      entities: [Artist, Music, Participant, ChartOfWeek, ChartOfDay, ChartOfHour, ChartOfMonth],
+      entities: [
+        Artist,
+        Music,
+        Participant,
+        ChartOfWeek,
+        ChartOfDay,
+        ChartOfHour,
+        ChartOfMonth,
+        ChartOfYear
+      ],
       synchronize: true
     }),
     TypeOrmModule.forFeature([Artist])
