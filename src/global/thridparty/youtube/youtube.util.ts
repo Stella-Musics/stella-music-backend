b46af049dc1a4bbe0745cdaf8bab20d330303b94
dolong2 <k01066624566@gmail.com>
2024-04-20 15:21:46 +0900

    [chore] getVideoInfo 에서 VideoInfoDto를 반환하도록 수정

--- src/global/thridparty/youtube/youtube.util.ts
+++ src/global/thridparty/youtube/youtube.util.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { google } from "googleapis";
+import { VideoInfoDto } from "./dto/video-info.dto";
 
 @Injectable()
 export class YoutubeUtils {
   constructor(private readonly configService: ConfigService) {}
 
-  async getVideoInfo(videoId: string) {
+  async getVideoInfo(videoIdList: string[]): Promise<VideoInfoDto[]> {
     const youtube = google.youtube({
       version: "v3",
       auth: this.configService.get<string>("YOUTUBE_SECRET_KEY")
@@ -14,9 +15,24 @@ export class YoutubeUtils {
 
     const response = youtube.videos.list({
       part: ["snippet", "statistics", "contentDetails"],
-      id: [videoId]
+      id: videoIdList
     });
 
-    console.log((await response).data.items);
+    const videoList = (await response).data.items;
+    if (videoList == null) throw new Error("영상 정보를 가져올 수 없음");
+
+    console.log(videoList);
+
+    const videoInfoList = videoList.map((video) => {
+      return new VideoInfoDto(
+        video.id ?? "",
+        video.snippet?.title ?? "",
+        video.snippet?.description ?? "",
+        video.statistics?.viewCount ?? "",
+        new Date(video.snippet?.publishedAt ?? "")
+      );
+    });
+
+    return videoInfoList;
   }
 }
