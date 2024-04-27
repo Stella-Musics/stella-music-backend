88dce4c4b7ad1f0786243d5edfcb27802012834b
dolong2 <k01066624566@gmail.com>
2024-04-27 21:48:32 +0900

    [add] 매주 차트를 갱신하는 스케줄러 추가

--- src/domain/music/util/music-info.scheduler.ts
+++ src/domain/music/util/music-info.scheduler.ts
@@ -10,6 +10,8 @@ import { ViewsOfDay } from "../entity/views-of-day.entity";
 import { ChartOfDay } from "src/domain/chart/entity/chart-of-day.entity";
 import { ViewsOfMonth } from "../entity/views-of-month.entity";
 import { ChartOfMonth } from "src/domain/chart/entity/chart-of-month.entity";
+import { ViewsOfWeek } from "../entity/views-of-week.entity";
+import { ChartOfWeek } from "src/domain/chart/entity/chart-of-week.entity";
 
 @Injectable()
 export class MusicInfoScheduler {
@@ -28,7 +30,11 @@ export class MusicInfoScheduler {
     @InjectRepository(ViewsOfMonth)
     private readonly viewsOfMonthRepository: Repository<ViewsOfMonth>,
     @InjectRepository(ChartOfMonth)
-    private readonly chartOfMonthRepository: Repository<ChartOfMonth>
+    private readonly chartOfMonthRepository: Repository<ChartOfMonth>,
+    @InjectRepository(ViewsOfWeek)
+    private readonly viewsOfWeekRepository: Repository<ViewsOfWeek>,
+    @InjectRepository(ChartOfWeek)
+    private readonly chartOfWeekRepository: Repository<ChartOfWeek>
   ) {}
 
   @Cron(CronExpression.EVERY_HOUR)
@@ -167,6 +173,53 @@ export class MusicInfoScheduler {
           views: viewsOfMonth.views,
           ranking: index + 1,
           rise: chartOfMonth.ranking - (index + 1)
+        }
+      );
+    });
+  }
+
+  @Cron(CronExpression.EVERY_WEEK)
+  async getMusicInfoEachWeek() {
+    const musicList = await this.musicRepository.find();
+
+    const musicIdList = musicList.map((music) => {
+      return music.id;
+    });
+
+    const musicInfoList = await this.youtubeUtils.getVideoInfos(musicIdList);
+
+    const viewsOfWeekList = await Promise.all(
+      musicInfoList.map(async (musicInfo) => {
+        const musicId = musicInfo.videoId;
+        const music = musicList.find((music) => music.id === musicId);
+        return await this.viewsOfWeekRepository.save({
+          views: musicInfo.viewCount,
+          music: music,
+          createdAt: Date()
+        });
+      })
+    );
+    viewsOfWeekList.sort((a: ViewsOfWeek, b: ViewsOfWeek) => b.views - a.views);
+
+    viewsOfWeekList.forEach(async (viewsOfWeek, index) => {
+      const chartOfWeek =
+        (await this.chartOfWeekRepository.findOneBy({ music: viewsOfWeek.music })) ??
+        (await this.chartOfWeekRepository.save({
+          music: viewsOfWeek.music,
+          views: viewsOfWeek.views,
+          ranking: index + 1,
+          rise: 0,
+          createdAt: Date()
+        }));
+
+      this.chartOfWeekRepository.update(
+        {
+          id: chartOfWeek.id
+        },
+        {
+          views: chartOfWeek.views,
+          ranking: index + 1,
+          rise: chartOfWeek.ranking - (index + 1)
         }
       );
     });
