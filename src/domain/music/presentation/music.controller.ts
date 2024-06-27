aaffaf06ed896c272d86a5f5cdedd9e8d63a7937
dolong2 <k01066624566@gmail.com>
2024-06-27 22:56:57 +0900

    [add] 차트 조회 엔드포인트 추가

--- src/domain/music/presentation/music.controller.ts
+++ src/domain/music/presentation/music.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, ParseEnumPipe, Query } from "@nestjs/common";
 import { MusicService } from "../service/music.service";
 import { MusicListResponse } from "../data/response/music-list.response";
 import { SortBy } from "../enum/sort-by.enum";
+import { ChartBy } from "../enum/chart-by.enum";
+import { MusicChartListResponse } from "../data/response/music-chart-list.response";
 
 @Controller("musics")
 export class MusicController {
@@ -12,5 +14,12 @@ export class MusicController {
     @Query("sortBy", new ParseEnumPipe(SortBy)) sortBy: SortBy
   ): Promise<MusicListResponse> {
     return await this.musicService.getMusic(sortBy);
+  }
+
+  @Get("chart")
+  async getMusicChart(
+    @Query("chartBy", new ParseEnumPipe(ChartBy)) chartBy: ChartBy
+  ): Promise<MusicChartListResponse> {
+    return await this.musicService.getMusicChart(chartBy);
   }
 }
