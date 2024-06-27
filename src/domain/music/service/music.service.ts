090ac420a154894b7e184733edd2220b5340fe44
dolong2 <k01066624566@gmail.com>
2024-06-27 22:56:30 +0900

    [add] 음악 차트 조회 로직 추가

--- src/domain/music/service/music.service.ts
+++ src/domain/music/service/music.service.ts
@@ -7,6 +7,10 @@ import { MusicListResponse } from "../data/response/music-list.response";
 import { SortBy } from "../enum/sort-by.enum";
 import { Participant } from "src/domain/participant/entity/participant.entity";
 import { ParticipantInfo } from "src/domain/participant/data/response/participant-info.response";
+import { ChartBy } from "../enum/chart-by.enum";
+import { GetChartUtil } from "src/domain/chart/util/get-chart.util";
+import { MusicChartListResponse } from "../data/response/music-chart-list.response";
+import { MusicChartResponse } from "../data/response/music-chart.response";
 
 @Injectable()
 export class MusicService {
@@ -14,7 +18,8 @@ export class MusicService {
     @InjectRepository(Music)
     private readonly musicRepository: Repository<Music>,
     @InjectRepository(Participant)
-    private readonly participantRepository: Repository<Participant>
+    private readonly participantRepository: Repository<Participant>,
+    private readonly getChartUtil: GetChartUtil
   ) {}
 
   async getMusic(sortBy: SortBy): Promise<MusicListResponse> {
@@ -54,6 +59,36 @@ export class MusicService {
     const musicResponseList = await this.transformParticipantsToMusicResponse(pariticipantList);
 
     return new MusicListResponse(musicResponseList);
+  }
+
+  async getMusicChart(chartBy: ChartBy): Promise<MusicChartListResponse> {
+    const chartList = (await this.getChartUtil.getChart(chartBy)).sort((a, b) => b.views - a.views);
+    const musicChartResponseList = await Promise.all(
+      chartList.map(async (chart) => {
+        const music = chart.music;
+        const pariticipantList = await this.participantRepository.find({
+          where: { music },
+          relations: ["artist"]
+        });
+        const participantInfoList = await Promise.all(
+          pariticipantList.map(async (participant) => {
+            return new ParticipantInfo(participant.artist.id, participant.artist.name);
+          })
+        );
+        return new MusicChartResponse(
+          music.id,
+          music.name,
+          music.youtubeId,
+          music.views,
+          music.uploadedDate,
+          music.TJKaraokeCode,
+          music.KYKaraokeCode,
+          chart.rise,
+          participantInfoList
+        );
+      })
+    );
+    return new MusicChartListResponse(musicChartResponseList);
   }
 
   private async transformParticipantsToMusicResponse(
