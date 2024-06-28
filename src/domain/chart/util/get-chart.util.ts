8f15d88629599eeaf89158c99f654ac11f72548b
dolong2 <k01066624566@gmail.com>
2024-06-28 23:36:36 +0900

    [chore] join을 통해서 차트 데이터를 가져오도록 수정

--- src/domain/chart/util/get-chart.util.ts
+++ src/domain/chart/util/get-chart.util.ts
@@ -6,8 +6,11 @@ import { Repository } from "typeorm";
 import { ChartOfHour } from "../entity/chart-of-hour.entity";
 import { ChartOfMonth } from "../entity/chart-of-month.entity";
 import { ChartOfWeek } from "../entity/chart-of-week.entity";
-// import { BaseChartEntity } from "../entity/base/base-chart.entity";
 import { ChartBy } from "src/domain/music/enum/chart-by.enum";
+import { Participant } from "src/domain/participant/entity/participant.entity";
+import { Artist } from "src/domain/artist/entity/artist.entity";
+import { MusicChartResponse } from "src/domain/music/data/response/music-chart.response";
+import { ParticipantInfo } from "src/domain/participant/data/response/participant-info.response";
 
 @Injectable()
 export class GetChartUtil {
@@ -24,9 +27,7 @@ export class GetChartUtil {
     private readonly chartOfWeekRepository: Repository<ChartOfWeek>
   ) {}
 
-  async getChart(
-    chartBy: ChartBy
-  ): Promise<ChartOfDay[] | ChartOfHour[] | ChartOfYear[] | ChartOfMonth[] | ChartOfWeek[]> {
+  async getChart(chartBy: ChartBy): Promise<MusicChartResponse[]> {
     const repositoryMap: {
       [key: string]: Repository<
         ChartOfDay | ChartOfHour | ChartOfYear | ChartOfMonth | ChartOfWeek
@@ -40,11 +41,42 @@ export class GetChartUtil {
     };
 
     const chartRepository = repositoryMap[chartBy];
-    const chartList = await chartRepository.find({
-      relations: ["music"],
-      order: { views: "DESC" }
-    });
+    const rawResult = await chartRepository
+      .createQueryBuilder("chart")
+      .leftJoinAndSelect("chart.music", "music")
+      .leftJoin(Participant, "participant", "participant.musicId = music.id")
+      .leftJoin(Artist, "artist", "participant.artistId = artist.id")
+      .select(["chart", "music", "participant.id", "artist.id", "artist.name"])
+      .orderBy("music.views", "DESC")
+      .getRawMany();
 
-    return chartList;
+    console.log(rawResult);
+
+    const chartMap = new Map<string, MusicChartResponse>();
+
+    await Promise.all(
+      rawResult.map(async (result) => {
+        const participantInfo = new ParticipantInfo(result.artist_id, result.artist_name);
+        if (chartMap.has(result.music_id)) {
+          chartMap.get(result.music_id)?.participantInfos.push(participantInfo);
+        } else {
+          const musicChartResponse = new MusicChartResponse(
+            result.music_id,
+            result.music_name,
+            result.music_youtubeId,
+            result.music_views,
+            result.uploadedDate,
+            result.TJKaraokeCode,
+            result.KYKaraokeCode,
+            result.chart_rise,
+            result.chart_ranking,
+            [participantInfo]
+          );
+          chartMap.set(result.music_id, musicChartResponse);
+        }
+      })
+    );
+
+    return Array.from(chartMap.values());
   }
 }
