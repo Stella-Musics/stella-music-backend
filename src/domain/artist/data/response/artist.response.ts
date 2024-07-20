9cad61184fc68efd65e064723751fd024418629a
dolong2 <k01066624566@gmail.com>
2024-07-20 13:07:47 +0900

    [add] 아티스트 응답 객체 추가

---
+++ src/domain/artist/data/response/artist.response.ts
@@ -0,0 +1,9 @@
+export class AritstResponse {
+  constructor(
+    readonly id: number,
+    readonly name: string,
+    readonly chzzkUrl: string,
+    readonly youtubeUrl: string,
+    readonly youtubeMusicUrl: string
+  ) {}
+}
