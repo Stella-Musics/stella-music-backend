ebacf3231d14a82f70d550b4a89a4daa1816ba15
dolong2 <k01066624566@gmail.com>
2024-07-21 18:27:25 +0900

    [chore] Artist 엔티티의 url필드를 ChannelUrl이랑 OneToMany로 수정

--- src/domain/artist/entity/artist.entity.ts
+++ src/domain/artist/entity/artist.entity.ts
@@ -1,5 +1,6 @@
 import { Participant } from "src/domain/participant/entity/participant.entity";
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ChannelUrl } from "./channel-url.entity";
 
 @Entity()
 export class Artist extends BaseEntity {
@@ -9,15 +10,9 @@ export class Artist extends BaseEntity {
   @Column()
   name: string;
 
-  @Column()
-  chzzkUrl: string;
-
-  @Column()
-  youtubeUrl: string;
-
-  @Column()
-  youtueMusicUrl: string | null;
-
   @OneToMany(() => Participant, (participant) => participant.artist)
   participants: Participant[];
+
+  @OneToMany(() => ChannelUrl, (channelUrl) => channelUrl.artist)
+  urls: ChannelUrl[];
 }
