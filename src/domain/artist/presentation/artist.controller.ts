826b31740499c19fc05b40646a9e7dd11d21c89e
dolong2 <k01066624566@gmail.com>
2024-07-21 17:10:55 +0900

    [add] 아티스트 조회 엔드포인트 추가

--- src/domain/artist/presentation/artist.controller.ts
+++ src/domain/artist/presentation/artist.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { ArtistService } from "../service/artist.service";
+import { AritstListResponse } from "../data/response/artist-list.response";
 
 @Controller("artists")
 export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
+
+  @Get()
+  async getArtists(): Promise<AritstListResponse> {
+    return await this.artistService.getArtist();
+  }
 }
