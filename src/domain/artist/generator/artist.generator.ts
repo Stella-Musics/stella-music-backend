56dac1b7dc33577d2265fbe62f4d53d0e632e603
dolong2 <k01066624566@gmail.com>
2024-08-18 16:24:41 +0900

    [chore] ArtistGenerator에서 몇기생인지 초기화하도록 수정

--- src/domain/artist/generator/artist.generator.ts
+++ src/domain/artist/generator/artist.generator.ts
@@ -3,6 +3,13 @@ import { Artist } from "../entity/artist.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 
+class ArtistInfo {
+  constructor(
+    readonly name: string,
+    readonly period: number | null
+  ) {}
+}
+
 @Injectable()
 export class ArtistGenerator implements OnModuleInit {
   constructor(
@@ -11,23 +18,24 @@ export class ArtistGenerator implements OnModuleInit {
   ) {}
 
   async onModuleInit() {
-    const artists = [
-      "스텔라이브",
-      "아야츠노 유니",
-      "아이리 칸나",
-      "시라유키 히나",
-      "아라하시 타비",
-      "네네코 마시로",
-      "아카네 리제",
-      "텐코 시부키",
-      "아오쿠모 린",
-      "하나코 나나",
-      "유즈하 리코"
+    const artistInfos = [
+      new ArtistInfo("스텔라이브", null),
+      new ArtistInfo("아야츠노 유니", 1),
+      new ArtistInfo("아이리 칸나", 1),
+      new ArtistInfo("시라유키 히나", 2),
+      new ArtistInfo("아라하시 타비", 2),
+      new ArtistInfo("네네코 마시로", 2),
+      new ArtistInfo("아카네 리제", 2),
+      new ArtistInfo("텐코 시부키", 3),
+      new ArtistInfo("아오쿠모 린", 3),
+      new ArtistInfo("하나코 나나", 3),
+      new ArtistInfo("유즈하 리코", 3)
     ];
 
-    artists.forEach(async (artist) => {
-      const isExist = await this.artistRepository.existsBy({ name: artist });
-      if (!isExist) await this.artistRepository.save({ name: artist });
+    artistInfos.forEach(async (artistInfo) => {
+      const isExist = await this.artistRepository.existsBy({ name: artistInfo.name });
+      if (!isExist)
+        await this.artistRepository.save({ name: artistInfo.name, period: artistInfo.period });
     });
   }
 }
