1d6df64dedc6b66db4247bbabf651ad8c7a78274
dolong2 <k01066624566@gmail.com>
2024-10-16 00:01:50 +0900

    [chore] redirect url을 환경변수에서 가져오도록 수정

--- src/domain/auth/controller/auth.controller.ts
+++ src/domain/auth/controller/auth.controller.ts
@@ -1,17 +1,24 @@
 import { Controller, Get, UseGuards, Res, Req } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { AuthGuard } from "@nestjs/passport";
 
 @Controller("auth")
 export class AuthController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get("google")
   @UseGuards(AuthGuard("google"))
   async googleLogin(): Promise<void> {}
 
-  @Get("google/redirect")
+  @Get("google/callback")
   @UseGuards(AuthGuard("google"))
   async googleLoginCallback(@Req() req, @Res() res): Promise<void> {
-    const jwt: string = req.user.jwt;
-    if (jwt) res.redirect("http://localhost:3000/login/success?jwt=" + jwt);
-    else res.redirect("http://localhost:3000/login/failure");
+    const accessToken: string = req.user.accessToken;
+    if (accessToken)
+      res.redirect(this.configService.get<string>("GOOGLE_SUCCESS_URL") + "?token=" + accessToken);
+    else
+      res.redirect(
+        this.configService.get<string>("GOOGLE_FAILURE_URL") + "?message=" + req.err.message
+      );
   }
 }
