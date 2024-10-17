1bb4ef0d447110f4abccf215b472e848f7b6c455
dolong2 <k01066624566@gmail.com>
2024-10-17 20:55:54 +0900

    [chore] jwt에서 accessExpiresIn 계산 로직 수정

--- src/global/jwt/jwt.generator.ts
+++ src/global/jwt/jwt.generator.ts
@@ -16,7 +16,8 @@ export class JwtGenerator {
     accessExpiresIn: Date;
   }> {
     const payload = { id: user.id, sub: user.socialId };
-    const accessExpiresIn = this.configService.get<number>("JWT_ACCESS_TIME");
+    const accessExpiresIn = this.configService.get<string>("JWT_ACCESS_TIME");
+    const accessExpiresInToInt = parseInt(accessExpiresIn === undefined ? "0" : accessExpiresIn);
     const accessToken = this.jwtService.sign(payload, {
       secret: this.configService.get<string>("JWT_ACCESS_SECRET"),
       expiresIn: accessExpiresIn
@@ -26,10 +27,11 @@ export class JwtGenerator {
       expiresIn: this.configService.get<string>("JWT_REFRESH_TIME")
     });
 
+    const currentDate = new Date();
     return {
       accessToken,
       refreshToken,
-      accessExpiresIn: new Date(Date.now() + (accessExpiresIn === undefined ? 0 : accessExpiresIn))
+      accessExpiresIn: new Date(currentDate.getTime() + accessExpiresInToInt)
     };
   }
 }
