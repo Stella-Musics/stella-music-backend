93d155b6db348cbe5d19700ec211b117283b23d4
dolong2 <k01066624566@gmail.com>
2024-10-17 23:22:37 +0900

    [chore] jwt 발급 로직 수정

--- src/global/jwt/jwt.generator.ts
+++ src/global/jwt/jwt.generator.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { JwtService } from "@nestjs/jwt";
 import { User } from "src/domain/user/entity/user.entity";
@@ -9,6 +9,19 @@ export class JwtGenerator {
     readonly jwtService: JwtService,
     private readonly configService: ConfigService
   ) {}
+  private createToken(
+    payload: any,
+    secretKey: string | undefined,
+    expiresIn: string | undefined
+  ): string {
+    if (!payload || !secretKey || !expiresIn)
+      throw new HttpException("서버 값 에러 입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    return this.jwtService.sign(payload, {
+      secret: secretKey,
+      expiresIn
+    });
+  }
 
   async generateToken(user: User): Promise<{
     accessToken: string;
@@ -17,15 +30,16 @@ export class JwtGenerator {
   }> {
     const payload = { id: user.id, sub: user.socialId };
     const accessExpiresIn = this.configService.get<string>("JWT_ACCESS_TIME");
-    const accessExpiresInToInt = parseInt(accessExpiresIn === undefined ? "0" : accessExpiresIn);
+    const accessExpiresInToInt = parseInt(accessExpiresIn || "0");
     const accessToken = this.jwtService.sign(payload, {
       secret: this.configService.get<string>("JWT_ACCESS_SECRET"),
       expiresIn: accessExpiresIn
     });
-    const refreshToken = this.jwtService.sign(payload, {
-      secret: this.configService.get<string>("JWT_REFRESH_SECRET"),
-      expiresIn: this.configService.get<string>("JWT_REFRESH_TIME")
-    });
+    const refreshToken = this.createToken(
+      payload,
+      this.configService.get<string>("JWT_REFRESH_SECRET"),
+      this.configService.get<string>("JWT_REFRESH_TIME")
+    );
 
     const currentDate = new Date();
     return {
