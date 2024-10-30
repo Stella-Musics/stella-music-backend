c22d78b597d6f98701346fe258f614444683360a
dolong2 <k01066624566@gmail.com>
2024-10-30 23:29:55 +0900

    [add] ArtistController에 swagger 옵션 추가

--- src/domain/artist/data/response/artist-list.response.ts
+++ src/domain/artist/data/response/artist-list.response.ts
@@ -1,5 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { ArtistResponse } from "./artist.response";
 
 export class ArtistListResponse {
-  constructor(readonly list: ArtistResponse[]) {}
+  constructor(list: ArtistResponse[]) {
+    this.list = list;
+  }
+
+  @ApiProperty({ type: ArtistResponse, description: "스텔라 조회 목록" })
+  readonly list: ArtistResponse[];
 }
--- src/domain/artist/data/response/artists-by-generation.response.ts
+++ src/domain/artist/data/response/artists-by-generation.response.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { ArtistResponse } from "./artist.response";
 
 export class ArtistByGenerationResponse {
+  @ApiProperty({ description: "'기수: 아티스트 리스트' 형식으로 응답" })
   artists: { [key: number]: ArtistResponse[] };
 
   constructor(artists: Map<number, ArtistResponse[]>) {
