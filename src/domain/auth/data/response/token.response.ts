342ce4512e253ea8f986cedf441cc0714ba069cb
dolong2 <k01066624566@gmail.com>
2024-10-30 23:33:50 +0900

    [add] AuthController에 swagger 옵션 추가

--- src/domain/auth/data/response/token.response.ts
+++ src/domain/auth/data/response/token.response.ts
@@ -1,7 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class TokenResponse {
-  constructor(
-    readonly accessToken: string,
-    readonly refreshToken: string,
-    readonly accessExpiresIn: Date
-  ) {}
+  constructor(accessToken: string, refreshToken: string, accessExpiresIn: Date) {
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+    this.accessExpiresIn = accessExpiresIn;
+  }
+
+  @ApiProperty({ description: "엑세스 토큰" })
+  readonly accessToken: string;
+  @ApiProperty({ description: "리프레시 토큰" })
+  readonly refreshToken: string;
+  @ApiProperty({ description: "엑세스 토큰 만료 기간" })
+  readonly accessExpiresIn: Date;
 }
