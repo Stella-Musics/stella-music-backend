502b6c6559094f0ba73c188a659fc43c3d8a5d0a
dolong2 <k01066624566@gmail.com>
2024-10-30 22:57:23 +0900

    [add] swagger 설정 메서드 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await configApiDoc(app);
   await app.listen(3000);
 }
 bootstrap();
+
+async function configApiDoc(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle("스텔 플레이 API 문서")
+    .setDescription("스텔 플레이의 API 문서입니다.")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api-docs", app, document);
+}
